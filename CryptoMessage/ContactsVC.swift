f12266f4c6f7d9acddc89cd00bc35db00c157d5d
stagiaire <stagiaire@iMac20.local>
2017-05-16 15:58:08 +0200

    multi edit view

---
+++ CryptoMessage/ContactsVC.swift
@@ -0,0 +1,47 @@
+//
+//  ContactsVC.swift
+//  CryptoMessage
+//
+//  Created by stagiaire on 16/05/2017.
+//  Copyright © 2017 stagiaire. All rights reserved.
+//
+
+import UIKit
+
+class ContactsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+} // class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- CryptoMessage/ViewController.swift
+++ CryptoMessage/ViewController.swift
@@ -7,17 +7,23 @@
 //
 
 import UIKit
+import MessageUI
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MFMessageComposeViewControllerDelegate {
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func messageComposeViewController(_ controller: MFMessageComposeViewController, didFinishWith result: MessageComposeResult)
+    {
+        
     }
 
 
