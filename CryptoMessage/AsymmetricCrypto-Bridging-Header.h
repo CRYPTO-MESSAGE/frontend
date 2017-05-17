79c3f0aefe6d18dc8766ca76e191a9df13bdc768
Daniel Gonçalves <daniel.gonc@lves.fr>
2017-05-17 10:03:49 +0200

    Add AsymmetricCrypto library

---
+++ CryptoMessage/AsymmetricCrypto-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  AsymmetricCrypto-Bridging-Header.h
+//  CryptoMessage
+//
+//  Created by stagiaire on 17/05/2017.
+//  Copyright © 2017 stagiaire. All rights reserved.
+//
+
+#ifndef AsymmetricCrypto_Bridging_Header_h
+#define AsymmetricCrypto_Bridging_Header_h
+#import <CommonCrypto/CommonCrypto.h>
+
+#endif /* AsymmetricCrypto_Bridging_Header_h */
