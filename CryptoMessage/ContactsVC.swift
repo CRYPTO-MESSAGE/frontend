c5e17216abd8ea8db14a2a62614aa110362a4f01
stagiaire <stagiaire@iMac20.local>
2017-05-17 16:30:07 +0200

    fix bug pods

--- CryptoMessage/ContactsVC.swift
+++ CryptoMessage/ContactsVC.swift
@@ -7,19 +7,39 @@
 //
 
 import UIKit
+import JSQMessagesViewController
 
-class ContactsVC: UIViewController {
+class ContactsVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var contactTable: UITableView!
+    
+    private let CELL_ID = "Cell";
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1;
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1;
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: CELL_ID, for: indexPath);
+        
+        cell.textLabel?.text = "THIS WORKS";
+        
+        return cell;
+        
+    }
+
     
     @IBAction func logout(_ sender: Any) {
         dismiss(animated: true, completion: nil);
---
+++ Pods/Target Support Files/Pods-CryptoMessageTests/Pods-CryptoMessageTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CryptoMessageTests : NSObject
+@end
+@implementation PodsDummy_Pods_CryptoMessageTests
+@end
